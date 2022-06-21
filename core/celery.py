2b72a71fd6909a07a71ec6048c3aded8c6e349d7
Asparukh Meirambekov <asparukh2001@gmail.com>
2022-06-22 02:24:47 +0600

    tes

--- core/celery.py
+++ core/celery.py
@@ -1,11 +1,11 @@
 import os
-import dotenv
+#import dotenv
 
-from celery import Celery
-env_file = os.path.join(os.path.dirname(os.path.dirname(
-    os.path.realpath(__file__))), '.env'
-)
-dotenv.read_dotenv(env_file)
+#from celery import Celery
+#env_file = os.path.join(os.path.dirname(os.path.dirname(
+#    os.path.realpath(__file__))), '.env'
+#)
+#dotenv.read_dotenv(env_file)
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
